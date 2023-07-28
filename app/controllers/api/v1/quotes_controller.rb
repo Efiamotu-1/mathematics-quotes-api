76a00074c3b0cc924ff31a020ab09bd6db482d69
Efiamotu-1 <musahabeebefy10@gmail.com>
2023-07-28 17:54:20 +0100

    add extra code to generate random quote

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::QuotesController < ApplicationController
     def index
-        @quote = Quote.order("RANDOM()").first
+        @quote = Quote.where(id: rand(1..100))
+        # The below code can also be used to generate a random quote
+        # @quote = Quote.order("RANDOM()").first
         render json: @quote
     end
 end
